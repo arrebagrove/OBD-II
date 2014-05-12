a2be190dc05849492ceae0a5d4b31d1f3e206348
Tony Tallman <tony.tallman@live.com>
2014-05-11 19:30:25 -0500

    Add test app for learning how to communicate with ELM327 device over Bluetooth.

---
+++ Elm327Test/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Elm327Test.Resources;
+
+namespace Elm327Test
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
---
+++ Elm327Test/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Phone.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.Networking.Proximity;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Elm327Test
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
+
+			Loaded += MainPage_Loaded;
+		}
+
+		async void MainPage_Loaded(object sender, RoutedEventArgs e)
+		{
+			PeerFinder.AlternateIdentities["Bluetooth:Paired"] = "";
+			IReadOnlyList<PeerInformation> pairedDevices = null;
+
+			try
+			{
+				pairedDevices = await PeerFinder.FindAllPeersAsync();
+			}
+			catch (Exception exception)
+			{
+				if (exception.HResult == -2147023729)
+				{
+					pairedDevices = new List<PeerInformation>().AsReadOnly();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			try
+			{
+				var obdlink = pairedDevices.SingleOrDefault(device => device.DisplayName == "OBDLink MX");
+
+				if (obdlink != null)
+				{
+					_socket = new StreamSocket();
+					await _socket.ConnectAsync(obdlink.HostName, "1");
+				}
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(exception.Message, exception.GetType().ToString(), MessageBoxButton.OK);
+			}
+		}
+
+		StreamSocket _socket;
+
+		private void SendButton_Click(object sender, RoutedEventArgs e)
+		{
+				string command = commandTextBox.Text + "\n";
+				commandTextBox.Text = string.Empty;
+				resultsTextBox.Text += command;
+
+			try
+			{
+				DataWriter writer = new DataWriter(_socket.OutputStream);
+				writer.WriteString(command);
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(exception.Message + "\n\n" + exception.StackTrace, exception.GetType().ToString(), MessageBoxButton.OK);
+			}
+
+			try
+			{
+				DataReader reader = new DataReader(_socket.InputStream);
+				string response = reader.ReadString(1);
+				resultsTextBox.Text += response;
+				response = reader.ReadString(1);
+				resultsTextBox.Text += response;
+				response = reader.ReadString(1);
+				resultsTextBox.Text += response;
+				response = reader.ReadString(1);
+				resultsTextBox.Text += response;
+				response = reader.ReadString(1);
+				resultsTextBox.Text += response;
+				response = reader.ReadString(1);
+				resultsTextBox.Text += response;
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(exception.Message + "\n\n" + exception.StackTrace, exception.GetType().ToString(), MessageBoxButton.OK);
+			}
+		}
+	}
+}
